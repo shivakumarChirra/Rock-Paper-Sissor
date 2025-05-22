bdfe215c33e7ec87373a77a19dbe7d6b36beedcb
shivakumarchirra <chirrashivakumar5@gmail.com>
2025-05-22 22:14:00 +0530

    Initial Commit

---
+++ Rock Paper Sissor/Rock_Paper_SissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_SissorApp.swift
+//  Rock Paper Sissor
+//
+//  Created by shivakumar chirra on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_SissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rock Paper SissorTests/Rock_Paper_SissorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_SissorTests.swift
+//  Rock Paper SissorTests
+//
+//  Created by shivakumar chirra on 22/05/25.
+//
+
+import Testing
+@testable import Rock_Paper_Sissor
+
+struct Rock_Paper_SissorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
